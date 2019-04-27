9815ee67d80943717da5e309dbfe190967a72e7d
ayd1zizou <ayd1_zizou@hotmail.com>
2019-04-26 20:15:46 -0600

    commit prueba satisfactoria

--- P3_AYD1_2019/src/p3_ayd1/AdministradorTest.java
+++ P3_AYD1_2019/src/p3_ayd1/AdministradorTest.java
@@ -6,15 +6,12 @@ import org.junit.Test;
 
 public class AdministradorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
 	
 	@Test
 	public void test_actualizar_solicitud() {
 		Administrador admin = new Administrador();
-		int res=admin.actualizar_solicitud("aprobado", 11, 1);
+		int res=admin.actualizar_solicitud("aprobado", 1, 1);
 		int esperado =1;
 		assertEquals(esperado, res);
 	}
